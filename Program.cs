a3b9ada05d4f1ba101ce41e66452b53e8cd7fd71
Marina <mvalpeters@gmail.com>
2023-02-12 19:46:12 +0300

    Working program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] GetNewArray (string [] Old, int max)
+{
+    string [] New = new string [Old.Length];
+    int j = 0;
+    for (int i = 0; i<Old.Length; i++)
+    {
+     if (Old[i].Length <= max)
+        {
+        New[j] = Old[i];
+        j ++;
+        }
+    }
+    int M = j;
+    return New;
+}
+
+
+void PrintArray (string [] Strings)
+{
+    for (int i = 0; i<Strings.Length; i++)
+    {
+     if (Strings[i] is not null) /*печатаются только заполненные элементы*/
+     {Console.Write(Strings[i]);
+     Console.Write(" | ");}
+    }
+}
+
+
+string [] a = {"Hello", "2", "world", ":-)"};
+Console.WriteLine("Заданный массив");
+PrintArray(a);
+Console.WriteLine(" ");
+Console.WriteLine("Полученный массив");
+string [] b = GetNewArray(a, 3);
+PrintArray(b);
+Console.WriteLine(" ");
